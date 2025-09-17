a30f1e34bee0e03c9d2fb14014289ed539421f43
phana07 <phanaphorn37@gmail.com>
2025-09-17 16:36:42 +0700

    add home and about

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 Route::get("/user", [PageController::class, 'users']);
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 Route::get('/about', function () {
     return view('about');
